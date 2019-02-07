67c098998dc2953a7957b0079a90dbaf1debc974
Marc Cavigli <mcavigli@gmail.com>
2019-02-07 21:32:26 +0000

    Accidentally uploaded the wrong file. Here is the correct .c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - print the alphabet in lowercase
+ *
+ *description: Will print out the entire alphabet in lowercase
+ *Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a == 'e' || a == 'q')
+		{
+			a++;
+		}
+		else
+		{
+		putchar(a);
+		a++;
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
