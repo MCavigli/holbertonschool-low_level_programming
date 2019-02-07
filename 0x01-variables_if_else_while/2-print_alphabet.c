e1d26a6f43ca43b012a1c97f68b37a0357db23c5
Marc Cavigli <mcavigli@gmail.com>
2019-02-07 21:09:46 +0000

    Pushed Task 2 to directory

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - print the alphabet in lowercase
+ *
+ *return: 0
+ */
+int main(void)
+{
+	char a;
+	for (a = 'a' ; a <= 'z' ; a++)
+		putchar(a);
+	return (0);
+}
