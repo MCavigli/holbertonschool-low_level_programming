92d101be5f5d4bc62a5d66df5e7bad109f3686c0
Marc Cavigli <mcavigli@gmail.com>
2019-02-07 21:22:17 +0000

    Added a line break

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,16 +2,19 @@
 /**
  *main - print the alphabet in lowercase
  *
+ *description: Will print out the entire alphabet in lowercase
  *return: 0
  */
 int main(void)
 {
 	char a = 'a';
+
 	while (a <= 'z')
 	{
 		putchar(a);
 		a++;
 	}
+
 	putchar('\n');
 	return (0);
 }
