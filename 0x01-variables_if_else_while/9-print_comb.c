f840161cf085520f930a10f7f754ccf563aeba80
Marc Cavigli <mcavigli@gmail.com>
2019-02-07 23:22:55 +0000

    Pushed Task 9 to directory

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *main - prints all the numbers of base 16 in lowercase
+ *
+ *Description: We can use putchar 3 times
+ *Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+       		putchar('0' + n);
+		if (n == 9)
+		{
+			putchar('\n');
+			break;
+		}
+		else
+		{
+			putchar(44);
+			putchar(' ');
+			n++;
+		}
+	}
+
+	return (0);
+}
