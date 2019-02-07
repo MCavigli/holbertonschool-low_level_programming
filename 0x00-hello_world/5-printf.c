f98880862a32c44c14f5a4d205f9dcdbb89d4a97
Marc Cavigli <mcavigli@gmail.com>
2019-02-07 00:05:07 +0000

    Added comments to code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
-
-int main (void)
+/**
+ *main - Uses printf to print a string
+ *
+ *Description: We have been tasked with using printf
+ *Return: Shouldn't return anything (0)
+ */
+int main(void)
 {
-
 	printf("with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
-
 }
