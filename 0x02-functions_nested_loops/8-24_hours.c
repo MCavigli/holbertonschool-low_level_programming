7837d879ba4dae1b4e28b4fca7194dc024c7932c
Marc Cavigli <mcavigli@gmail.com>
2019-02-12 06:34:46 +0000

    Pushed Task 8 to directory

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *jack_bauer - prints every minute of the day, from 00:00 to 23:59
+ *
+ *Return: nothing
+ */
+
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
