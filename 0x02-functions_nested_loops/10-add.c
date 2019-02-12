1df5382d8fe8ba66745052b66418a599b29ecc94
Marc Cavigli <mcavigli@gmail.com>
2019-02-12 20:22:55 +0000

    Pushed Task 10 to directory

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * add - adds two integers
+ *
+ * @n: first integer
+ * @m: second integer
+ *
+ * Return: sum of integers
+ */
+int add(int n, int m)
+{
+	int sum = n + m;
+
+	return (sum);
+}
