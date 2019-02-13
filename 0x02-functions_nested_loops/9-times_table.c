f267e9853904d0401396b7389909da119e9a04f7
Marc Cavigli <mcavigli@gmail.com>
2019-02-13 05:36:20 +0000

    Pushed Task 9 to the directory

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: nothing
+ */
+
+void times_table(void)
+{
+	int i, j;
+	int k = 0;
+	int p;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 11; j++)
+		{
+			p = j * k;
+
+			if (p >= 0 && p <= 9)
+			{
+				if (j == 0)
+					_putchar(p + '0');
+				else if (j == 10)
+				{
+					_putchar('\n');
+					k++;
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(p + '0');
+				}
+			}
+			else if (p >= 10)
+			{
+				if (j == 10)
+				{
+					_putchar('\n');
+					k++;
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((p / 10) + '0');
+					_putchar((p % 10) + '0');
+				}
+			}
+
+		}
+	}
+}
