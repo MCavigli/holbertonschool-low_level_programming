eb24ff1cb97163440d2c7ebd72c14a6f3c1bd684
Marc Cavigli <mcavigli@gmail.com>
2019-02-14 23:08:26 +0000

    Pushed Task 5 to directory

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -9,26 +9,16 @@ void more_numbers(void)
 {
 	int i;
 	int j;
-	int k;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j + '0');
+			if (j > 9)
+				_putchar(j / 10 + '0');
 
-			if (j == 9)
-			{
-				for (k = 0; k <= 4; k++)
-				{
-					_putchar('1');
-					_putchar(k + '0');
-				}
-			}
-
+			_putchar(j % 10 + '0');
 		}
-
-
 		_putchar('\n');
 	}
 }
