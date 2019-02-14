e508b9cdf5644b5290f75efc36483edd7255139b
Marc Cavigli <mcavigli@gmail.com>
2019-02-14 19:17:28 +0000

    Update header, push Task 2 to directory.

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two integers
+ *
+ * @a: the first integer to multiply
+ * @b: the second integer to multiply
+ *
+ * Return: the result
+ */
+
+int mul(int a, int b)
+{
+	int mul = a * b;
+
+	return (mul);
+}
