90107cbd40502049ba9b7ae7002019cbbfcc5d67
Marc Cavigli <mcavigli@gmail.com>
2019-02-14 19:03:52 +0000

    Update header, pushed Task 1 to directory

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ *
+ * @c: the digit or character to check
+ *
+ * Return: 1 if @c is a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
