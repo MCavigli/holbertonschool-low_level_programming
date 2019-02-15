9a56aac38a4aa9630d48647aba8de2c2014a1af0
Marc Cavigli <mcavigli@gmail.com>
2019-02-15 04:39:07 +0000

    Pushed Task 9 to directory

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Prints 1-100. Replaces multiples of 3 with 'fizz', of 5 with 'buzz'
+ * and of 3 & 5 with 'FizzBuzz'
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	char fizz[] = "Fizz";
+	char buzz[] = "Buzz";
+	char fizzbuzz[] = "FizzBuzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+
+		if (i % 3 == 0 && i % 5 == 0)
+		printf("%s ", fizzbuzz);
+		else if ((i % 3) == 0)
+			printf("%s ", fizz);
+		else if ((i % 5) == 0)
+			printf("%s ", buzz);
+		else
+			printf("%i ", i);
+	}
+	putchar('\n');
+	return (0);
+}
