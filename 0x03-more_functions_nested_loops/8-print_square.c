07c3da214ea3e0f32aaa0976ded0cf8a2027d305
Marc Cavigli <mcavigli@gmail.com>
2019-02-15 04:23:12 +0000

    Added conditional to account for size == 0

--- 0x03-more_functions_nested_loops/8-print_square.c
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -11,13 +11,18 @@ void print_square(int size)
 	int i;
 	int j;
 
-	for (i = 1; i <= size; i++)
+	if (size > 0)
 	{
-		for (j = 1; j <= size; j++)
+		for (i = 1; i <= size; i++)
 		{
-			_putchar(35);
-			if (j == size)
-				_putchar('\n');
+			for (j = 1; j <= size; j++)
+			{
+				_putchar(35);
+				if (j == size)
+					_putchar('\n');
+			}
 		}
 	}
+	else
+		_putchar('\n');
 }
