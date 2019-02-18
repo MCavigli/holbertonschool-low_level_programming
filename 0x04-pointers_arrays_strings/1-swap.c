3a069ee6c3af2164110c0eccbf9d84ebbb0a4450
Marc Cavigli <mcavigli@gmail.com>
2019-02-18 23:37:54 +0000

    Pushed Task 1 to directory

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
