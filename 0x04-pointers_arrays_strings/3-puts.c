20504fa1ebb0ddc34ef27c00ef36ec83ce08d68c
Marc Cavigli <mcavigli@gmail.com>
2019-02-19 00:07:01 +0000

    Pushed Task 3 to directory

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _puts - prints out a string, followed by a new line
+ *
+ * @str: the string to be printed out
+ *
+ * Return: the string
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
