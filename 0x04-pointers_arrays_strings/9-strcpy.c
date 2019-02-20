f0639ced1e9c488e752459008c9d4955f528f7bb
Marc Cavigli <mcavigli@gmail.com>
2019-02-20 04:43:35 +0000

    Added a null element at the end of the string

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -13,10 +13,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[i] <= '\0')
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
+	dest[i] = '\0';
+
 	return (dest);
 }
