daf9876105c607d07c4ae0ded8c884b0fac832ad
Marc Cavigli <mcavigli@gmail.com>
2019-02-21 23:19:04 +0000

    Completely rewrote code

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -10,21 +10,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int sum1 = 0; /* sume of s1's ASCII values */
-	int sum2 = 0; /* sum of s2's ASCII values */
-	int i, j; /* counter for s1 and s2 */
+	int i = 0;
 
-	for (i = 0; s1[i]; i++)
-		sum1 += s1[i];
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] || s2[i] == '\0')
+			break;
+		else
+			i++;
+	}
 
-	for (j = 0; s2[j]; j++)
-		sum2 += s2[j];
-
-	if (sum1 == sum2)
-		return (0);
-	else if (sum1 < sum2)
-		return (sum1 - sum2);
-	else
-		return (sum2 - sum1);
-
+	return (s1[i] - s2[i]);
 }
