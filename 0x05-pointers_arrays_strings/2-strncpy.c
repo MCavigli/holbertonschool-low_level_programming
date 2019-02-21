065f51beda1d9d482f4cf293bd3adf859d8eabff
Marc Cavigli <mcavigli@gmail.com>
2019-02-21 21:38:48 +0000

    Pushed Task 2 to the directory

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * _strncpy - copies a string to another string
+ *
+ * @dest: the buffer
+ * @src: the string to be copied
+ * @n: the number of bytes to include in @dest
+ *
+ * Return: pointer to the resulting string @dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len1; /* string length for src */
+	int len2; /* string length for dest */
+	int i;
+
+	len1 = 0;
+	len2 = 0;
+
+	while (src[len1] != '\0')
+		len1++;
+
+	while (dest[len2] != '\0')
+		len2++;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
