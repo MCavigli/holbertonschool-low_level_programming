e705a38d8767caeb00689dbc0233b010570f9eb0
Marc Cavigli <mcavigli@gmail.com>
2019-02-21 22:44:26 +0000

    Rewrote code after learning they're asking aobut ASCII values

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -10,22 +10,21 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int lens1; /* string length for s1 */
-	int lens2; /* string length for s2 */
+	int sum1 = 0; /* sume of s1's ASCII values */
+	int sum2 = 0; /* sum of s2's ASCII values */
+	int i, j; /* counter for s1 and s2 */
 
-	lens1 = 0;
-	lens2 = 0;
+	for (i = 0; s1[i]; i++)
+		sum1 += s1[i];
 
-	while (s1[lens1] != '\0')
-		lens1++;
+	for (j = 0; s2[j]; j++)
+		sum2 += s2[j];
 
-	while (s2[lens2] != '\0')
-		lens2++;
-
-	if (lens1 == lens2)
+	if (sum1 == sum2)
 		return (0);
-	else if (lens1 < lens2)
-		return (-15);
+	else if (sum1 < sum2)
+		return (sum1 - sum2);
 	else
-		return (15);
+		return (sum2 - sum1);
+
 }
