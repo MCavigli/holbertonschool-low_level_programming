e597cf29a80f3f098b52062a4c66f92a191a12a7
Marc Cavigli <mcavigli@gmail.com>
2019-02-22 03:43:06 +0000

    Pushed Task 7 to directory

---
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * leet - replaces a bunch of numbers with letters
+ *
+ * @z: the string to look at
+ *
+ * Return: the new string
+ */
+
+char *leet(char *z)
+{
+	char arr[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char replace[] = {'4', '3', '0', '7', '1'};
+	int i, j;
+
+	for (i = 0; z[i] != '\0'; i++)
+	{
+		for (j = 0; arr[j] != '\0'; j++)
+		{
+			if (z[i] == arr[j])
+				z[i] = replace[j / 2];
+		}
+	}
+	return (z);
+}
