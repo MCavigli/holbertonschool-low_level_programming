749228624d57a9ee2d3bde597152a9c7776dc823
Marc Cavigli <mcavigli@gmail.com>
2019-02-22 00:04:25 +0000

    Pushed Task 4 to directory

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * reverse_array - reverse the content of an array of integers
+ *
+ * @a: the array to reverse
+ * @n: the number of elements in the array
+ *
+ * Return: nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp; /* holds value */
+	int i;    /* counter */
+	int j = n -1; /* reverse counter */
+
+	for (i = 0; i < j; i++)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+		j--;
+	}
+
+
+}
