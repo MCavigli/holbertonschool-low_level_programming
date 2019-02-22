1fb740b809496a687a0c637662c66280ff6d31f4
Marc Cavigli <mcavigli@gmail.com>
2019-02-22 07:22:08 +0000

    Pushed Task 100 to directory

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_number - Prints an integer
+ *
+ * @n: the integer to print
+ *
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
+}
