bb036e12c19a2cb221bb608ee508fb064b69fdef
Marc Cavigli <mcavigli@gmail.com>
2019-02-22 07:11:48 +0000

    Changed location of j initialization

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -10,16 +10,16 @@
 char *cap_string(char *z)
 {
 	int i = 0;
-	int j = 0;
-	char badBoys[] = {' ', '\n', '\t', ',', ';', '.', '!', '?', '"', '(',
-			  ')', '{', '}'};
+	int j;
+	char badBoys[] = " \n\t,;.!?\"(){}";
+
 
 	if (z[0] >= 'a' && z[0] <= 'z')
 		z[0] -= 32;
 
 	for (; z[i] != '\0'; i++)
 	{
-		for (; j < 13; j++)
+		for (j = 0; j < 14; j++)
 		{
 			if (z[i] == badBoys[j])
 			{
