40ef25ad0bab5b2cef9fe87d772db18fc0101d4e
Marc Cavigli <mcavigli@gmail.com>
2019-02-22 06:36:50 +0000

    Removed conditional statement

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -10,18 +10,15 @@
 char *rot13(char *z)
 {
 	int i, j;
-	char a[] = "abcdefghijklmABCDEFGHIJKLM";
-	char n[] = "nopqrstuvwxyzNOPQRSTUVWXYZ";
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char n[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	for (i = 0; z[i] != '\0'; i++)
 	{
-		for (j = 0; j < 28; j++)
+		for (j = 0; j < 56; j++)
 		{
 			if (z[i] == a[j])
 				z[i] = n[j];
-
-			else if (z[i] == n[j])
-				z[i] = a[j];
 		}
 	}
 
