6f20d2c669368a4b571718b344781fe1573803d0
Marc Cavigli <mcavigli@gmail.com>
2019-02-25 22:51:02 +0000

    Pushed Task 3 to directory

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ *
+ * @s: segment to return bytes from
+ * @accept: the bytes to include
+ *
+ * Return: the number of bytes in the initial segment of @s which consist only
+ * of bytes from @accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int c = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] != 32)
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					c++;
+			}
+		}
+		else
+			return (c);
+
+	}
+	return (c);
+}
