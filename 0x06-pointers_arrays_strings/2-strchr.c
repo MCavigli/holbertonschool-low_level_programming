f3d8f885107a2a73fa73fcdc545fc485f9dd1518
Marc Cavigli <mcavigli@gmail.com>
2019-02-25 21:58:24 +0000

    Rewrote code

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,13 @@
 
 char *_strchr(char *s, char c)
 {
-
-	while (*s != '\0')
+	while (*s)
 	{
 		if (*s == c)
 		{
 			return (s);
 		}
-		else if ((*s + 1) == '\0')
+		else if (*s == '\0')
 			return ('\0');
 		else
 			s++;
