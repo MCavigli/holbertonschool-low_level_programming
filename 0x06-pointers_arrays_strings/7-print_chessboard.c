56ada4bd842a7e4c7c77abb7f00fa392d8c6ad2a
Marc Cavigli <mcavigli@gmail.com>
2019-02-26 00:29:46 +0000

    Pushed Chessboard task to directory

---
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_chessboard - prints the chessboard
+ *
+ * @a: the row of the array
+ *
+ * Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			if (j == 7)
+			{
+				_putchar(a[i][j]);
+				_putchar('\n');
+			}
+			else
+				_putchar(a[i][j]);
+		}
+	}
+}
