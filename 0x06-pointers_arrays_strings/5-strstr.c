7feefcfbde8dd2b83122d84fc2706ec41b3cb2ad
Marc Cavigli <mcavigli@gmail.com>
2019-02-26 23:12:30 +0000

    Fixed syntax to fit with betty checker

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -24,12 +24,11 @@ char *_strstr(char *haystack, char *needle)
 		{
 			if (haystack[i] != needle[i])
 				break;
-
 		}
-		if (i == s)
-			return (haystack);
-		else
+		if (i != s)
 			haystack++;
+		else
+			return (haystack);
 	}
 	return (NULL);
 }
