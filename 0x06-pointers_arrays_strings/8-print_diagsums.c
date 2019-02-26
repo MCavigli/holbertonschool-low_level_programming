cf59c57c3d4138223fc810c666c0fee670be0c40
Marc Cavigli <mcavigli@gmail.com>
2019-02-26 06:45:01 +0000

    Pushed Task 8 to directory

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square
+ * matrix of integers
+ *
+ * @a: the name of the array
+ * @size: the size of the array
+ *
+ * Return: nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int j = size - 1;
+	int sum1, sum2;
+
+	while (i <= (size * size))
+	{
+		sum1 = sum1 + a[i];
+		i = i + size + 1;
+	}
+
+	while (j < (size * size - 1))
+	{
+		if (a[j] != (size * size - 1))
+		{
+			sum2 += a[j];
+			j = j - 1 + size;
+		}
+		else
+			break;
+	}
+
+	printf("%i, %i\n", sum1, sum2);
+}
