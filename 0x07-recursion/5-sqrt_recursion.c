23cb281f087d3264786300fa619700966b0b74fe
Marc Cavigli <mcavigli@gmail.com>
2019-02-27 23:24:34 +0000

    Pushed Task 5 to directory

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: the number to return the square root of
+ *
+ * Return: the square root of @n
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (helper(n, n - 1));
+}
+
+/**
+ * helper - Finds the square root of two numbers
+ *
+ * @a: The number
+ * @b: The number to test for the square root of @a
+ *
+ * Return: square root
+ */
+
+int helper(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+
+	if (b * b < a)
+		return (-1);
+
+	else
+		return (helper(a, b - 1));
+}
