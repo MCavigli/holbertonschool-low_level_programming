8b8f3354d1300b1986887bb9aef6bb1ee40e5378
Marc Cavigli <mcavigli@gmail.com>
2019-02-27 18:48:01 +0000

    Pushed Task 0 to directory

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ *
+ * @s: The string that prints
+ *
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
