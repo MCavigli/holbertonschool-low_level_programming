39d9aeed7e1eff921d0114cb1d66bdbf1933aa9d
Marc Cavigli <mcavigli@gmail.com>
2019-02-28 07:33:35 +0000

    Updated header and pushed Task 7 to directory

--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -8,3 +8,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int helper(int a, int b);
+int primeFinder(int a, int b);
+int palindromeChecker(char *str, int len, int i);
