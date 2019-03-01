7a1fd17003c871e7bd98f4a0e1ca17f8fcd44515
Marc Cavigli <mcavigli@gmail.com>
2019-03-01 23:11:49 +0000

    Pushed Task 0 to directory

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints its name, followed by a new line
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (argc);
+}
