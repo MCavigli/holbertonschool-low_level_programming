88d8fef5e62afe81f26961d0954ac091660bb765
Marc Cavigli <mcavigli@gmail.com>
2019-03-02 21:11:20 +0000

    Pushed Task 2 to directory

---
+++ 0x09-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int c;
+	(void)argv;
+
+	for (c = 0; c < argc; c++)
+		printf("%s\n", argv[c]);
+
+	return (0);
+}
