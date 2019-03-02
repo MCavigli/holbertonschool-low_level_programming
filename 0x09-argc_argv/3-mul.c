397ec12674392a9a19df79f3443ada2c7a57d251
Marc Cavigli <mcavigli@gmail.com>
2019-03-02 23:23:59 +0000

    Changed %d to %i

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 {
 	int n = 0;
 	int m = 0;
-	int sum = 0;
+	int diff = 0;
 
 	if (argc != 3)
 	{
@@ -26,9 +26,9 @@ int main(int argc, char *argv[])
 	{
 		n = atoi(argv[1]);
 		m = atoi(argv[2]);
-		sum = n * m;
+		diff = n * m;
 
-		printf("%d\n", sum);
+		printf("%i\n", diff);
 	}
 
 	return (0);
