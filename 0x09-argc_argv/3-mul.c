970a146dc9170da7bbf1fff67e61e4d330cb9306
Marc Cavigli <mcavigli@gmail.com>
2019-03-02 23:14:48 +0000

    Pushed Task 3 to directory

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int n = 0;
+	int m = 0;
+
+	if (argc != 3)
+		printf("ERROR\n");
+
+	else
+	{
+		n = atoi(argv[1]);
+		m = atoi(argv[2]);
+
+		printf("%d\n", n * m);
+	}
+
+	return (0);
+}
