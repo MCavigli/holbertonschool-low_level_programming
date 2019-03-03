ce74b87af7f1a1a1e514463db96825ed00bb49b0
Marc Cavigli <mcavigli@gmail.com>
2019-03-03 23:59:10 +0000

    Added include statements in header

--- 0x08-static_libraries/holberton.h
+++ 0x08-static_libraries/holberton.h
@@ -1,3 +1,6 @@
+#ifndef _HOLBERTON_HEADER
+#define _HOLBERTON_HEADER
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +21,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
