353fa49fe05e01cb13edcfbd6523754cbeb5a031
Marc Cavigli <mcavigli@gmail.com>
2019-03-04 03:22:33 +0000

    Pushed task 4 to directory

---
+++ 0x09-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - adds positive numbers
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 1 if ERROR such as symbols that are not numbers,
+ * 0 if nothing is passed
+ */
+
+int main(int argc, char *argv[])
+{
+	int c;
+	int n = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (c = 1; c < argc; c++)
+	{
+		if (*argv[c] < '0' || *argv[c] > '9')
+		{
+			printf("ERROR\n");
+			return (1);
+		}
+		else
+			n = n + atoi(argv[c]);
+	}
+	printf("%d\n", n);
+
+	return (0);
+}
