982cd99826330cc3d233a8d7369b2f927fb2cbf3
Marc Cavigli <mcavigli@gmail.com>
2019-03-05 08:06:01 +0000

    Used multi-dimensional array to solve problem

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -14,25 +14,27 @@
 
 int main(int argc, char *argv[])
 {
-	int c;
+	int c, d;
 	int n = 0;
 
 	if (argc < 2)
-		printf("0\n");
-
-	else
 	{
-		for (c = 1; c < argc; c++)
+		printf("0\n");
+		return (0);
+	}
+
+	for (c = 1; c < argc; c++)
+	{
+		for (d = 0; argv[c][d] != '\0'; d++)
 		{
-			if (*argv[c] < '0' || *argv[c] > '9')
+			if (argv[c][d] < '0' || argv[c][d] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
-			else
-				n = n + atoi(argv[c]);
 		}
-		printf("%d\n", n);
+		n += atoi(argv[c]);
 	}
+	printf("%d\n", n);
 	return (0);
 }
