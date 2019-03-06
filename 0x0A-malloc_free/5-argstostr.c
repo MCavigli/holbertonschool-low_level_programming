00eab194c6dde383cc39a5bfe317ebe1f86bf3a8
Marc Cavigli <mcavigli@gmail.com>
2019-03-06 04:38:35 +0000

    Changed conditional

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -21,17 +21,20 @@ char *argstostr(int ac, char **av)
 	int m;
 	int counter = 0;
 
-	if (ac == 0 || av == 0)
+	if (ac == 0)
+		return (NULL);
+	if (av == NULL)
 		return (NULL);
 
 	for (i = 1; i <= ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 			counter++;
+
 		counter++;
 	}
 	counter++; /* to account for \n that needs to be added later */
-	ar = malloc(sizeof(char) * counter + 1); /* + 1 to account for \0 */
+	ar = malloc(sizeof(char) * counter);
 	if (ar == NULL)
 		return (NULL);
 
@@ -39,11 +42,11 @@ char *argstostr(int ac, char **av)
 	{
 		for (m = 0; av[n][m] != '\0'; m++)
 		{
-			ar[k] = av[n][m];
 			k++;
+			ar[k] = av[n][m];
 		}
-		ar[k] = '\n';
 		k++;
+		ar[k] = '\n';
 	}
 	ar[k] = '\0';
 	return (ar);
