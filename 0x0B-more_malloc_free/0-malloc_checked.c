3e9ac9fa04e37d4408baca0eae94dbd0d0aa9a7b
Marc Cavigli <mcavigli@gmail.com>
2019-03-07 20:18:47 +0000

    Pushed Task 1 to directory

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * @b: the number to allocate memory for
+ *
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *i;
+
+	i = malloc(b);
+	if (i == NULL)
+		exit (98);
+	return (i);
+}
