fdca7fce74e8b7d307ff67b0fc8b6edbfb1e38d6
Marc Cavigli <mcavigli@gmail.com>
2019-03-07 23:23:46 +0000

    Added a +1 to malloc

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ar = malloc(sizeof(int) * (max - min));
+	ar = malloc(sizeof(int) * (max - min + 1));
 	if (ar == NULL)
 		return (NULL);
 
