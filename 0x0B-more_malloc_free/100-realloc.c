98cdf68303787c96ad9e857b9241c7405adb4297
Marc Cavigli <mcavigli@gmail.com>
2019-03-08 04:34:06 +0000

    Changed order of conditionals, removed = from <=

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -21,22 +21,22 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (old_size == new_size)
 		return (ptr);
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
 	if (ptr == NULL)
 	{
 		ar = malloc(new_size);
 		return (ar);
+	}
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
 	}
 
 	ar = malloc(new_size);
 	if (ar == NULL)
 		return (NULL);
 
-	for (i = 0; i <= old_size; i++)
+	for (i = 0; i < old_size; i++)
 		ar[i] = oldar[i];
 
 	free(oldar);
