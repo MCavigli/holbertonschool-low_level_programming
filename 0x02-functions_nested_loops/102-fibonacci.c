0f2123f4cad5669b62cf81640850c72391f68d1f
Marc Cavigli <mcavigli@gmail.com>
2019-03-10 06:08:18 +0000

    Pushed Task 14 to directory

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k;
+	int counter;
+
+	i = 1;
+	j = 1;
+	k = 0;
+
+
+	for (counter = 0; counter <= 50; counter++)
+	{
+		if (counter == 50)
+			printf("%i\n", i);
+
+		printf("%i, ", i);
+		k = i + j;
+		j = i;
+		i = k;
+	}
+	return (0);
+}
