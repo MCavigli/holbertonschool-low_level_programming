37fdd987927d93ca8275b39bfcef23a516b3d38e
Marc Cavigli <mcavigli@gmail.com>
2019-03-11 20:42:41 +0000

    Changed output as per directions for (nil)

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -13,15 +13,15 @@ void print_dog(struct dog *d)
 		if (d->name)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Name: nil\n");
+			printf("Name: (nil)\n");
 		if (d->age)
 			printf("Age: %.6f\n", d->age);
 		else
-			printf("Age: nil\n");
+			printf("Age: (nil)\n");
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("Owner: nil\n");
+			printf("Owner: (nil)\n");
 
 	}
 	else
