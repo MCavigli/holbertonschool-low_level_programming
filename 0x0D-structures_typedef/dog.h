5ea5cc4add7fe0ad94764fd882599830056d60c9
Marc Cavigli <mcavigli@gmail.com>
2019-03-11 20:11:27 +0000

    Removed extra prototypes

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+/**
+ * struct dog - describes the sweet boy or girl
+ * @name: name of dog
+ * @age: how old is that dog?
+ * @owner: who is the lucky person that gets to live with that dog?
+ */
 struct dog
 {
 	char *name;
@@ -7,8 +13,4 @@ struct dog
 	char *owner;
 };
 int _putchar(char c);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 #endif
