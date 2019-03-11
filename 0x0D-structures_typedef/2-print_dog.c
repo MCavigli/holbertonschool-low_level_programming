caeee76b65198fc4385ec25cbbf0692a8e58365e
Marc Cavigli <mcavigli@gmail.com>
2019-03-11 20:37:31 +0000

    Pushed Task 2 to directory

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - prints a struct dog
+ * @d: the structure
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: nil\n");
+		if (d->age)
+			printf("Age: %.6f\n", d->age);
+		else
+			printf("Age: nil\n");
+		if (d->owner)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: nil\n");
+
+	}
+	else
+		printf(" ");
+}
