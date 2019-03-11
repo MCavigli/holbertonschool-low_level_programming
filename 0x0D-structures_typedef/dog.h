9176c63b15fa56efbd386c82a5d75ab65c1dc40c
Marc Cavigli <mcavigli@gmail.com>
2019-03-11 20:00:55 +0000

    Included struct dog

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
