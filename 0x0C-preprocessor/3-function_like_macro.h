0d31d00b2da488ad5f9022c4446bf739ee5e510d
Marc Cavigli <mcavigli@gmail.com>
2019-03-11 19:20:09 +0000

    Added H_

--- 0x0C-preprocessor/3-function_like_macro.h
+++ 0x0C-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_
-#define _3_FUNCTION_LIKE_MACRO_
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
 #define ABS(x) ((x) >= (0) ? (x): (-x))
 #endif
