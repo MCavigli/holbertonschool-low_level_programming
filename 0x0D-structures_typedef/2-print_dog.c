a9e7296fdfedc8d4d59a47e71b0fb466a5e37977
Marc Cavigli <mcavigli@gmail.com>
2019-03-11 20:48:22 +0000

    Added a nothing conditional

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
-		if (d->age)
+		if (d->age == d->age)
 			printf("Age: %.6f\n", d->age);
 		else
 			printf("Age: (nil)\n");
