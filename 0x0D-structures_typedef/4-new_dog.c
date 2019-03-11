43479c770f8527dbd531c6a28a28c660035cddd3
Marc Cavigli <mcavigli@gmail.com>
2019-03-11 21:55:15 +0000

    Pushed Task 4 to directory

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: The string to copy
+ * Return: a pointer to the duplicated string, NULL if insufficient memory
+ * or if @str is NULL
+ */
+char *_strdup(char *str)
+{
+	char *ar;
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+		i++;
+	ar = malloc(sizeof(char) * (i + 1));
+	if (ar == NULL)
+		return (NULL);
+	while (str[j])
+	{
+		ar[j] = str[j];
+		j++;
+	}
+	ar[j + 1] = 0;
+	return (ar);
+}
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: NULL if function fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+	char *ncpy;
+	char *ocpy;
+
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
+		return (NULL);
+
+	ncpy = _strdup(name);
+	ocpy = _strdup(owner);
+
+	new->name = ncpy;
+	new->age = age;
+	new->owner = ocpy;
+	return (new);
+}
