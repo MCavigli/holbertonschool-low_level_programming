04297eaa2a682f8d75b0b801912133265a04e483
Marc Cavigli <mcavigli@gmail.com>
2019-03-13 21:44:21 +0000

    Added conditional

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -18,10 +18,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
+	if (!array || !cmp)
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		if (p(array[i]) && array[i] != '0')
+		if (p(array[i]) && array[i] != 0)
 			break;
 		if (i == size - 1)
 			return (-1);
