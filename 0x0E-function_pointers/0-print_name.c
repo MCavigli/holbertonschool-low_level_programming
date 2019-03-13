c4e3bff2793d5c6abbe716bc99dad19aa7759a19
Marc Cavigli <mcavigli@gmail.com>
2019-03-13 20:21:40 +0000

    Pushed Task 0 to directory

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: the name to print
+ * @f: the function to send the name to
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
