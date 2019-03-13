0511068a5b363b259e4edfc10b212e1ff1f57701
Marc Cavigli <mcavigli@gmail.com>
2019-03-13 20:26:06 +0000

    Updated header

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,4 +1,5 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
