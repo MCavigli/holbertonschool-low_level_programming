6af498a25ded686dd5c80c79b209fe892ea7953f
Marc Cavigli <mcavigli@gmail.com>
2019-03-13 21:20:40 +0000

    Pushed Task 2 to directory

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: array to search through
+ * @size: number of elements in the array
+ * @cmp: pointer to the function to be used to compare values
+ * Return: the index of the first element for which the cmp function
+ * does not return 0;
+ * if no element matches, return -1;
+ * if size <= 0, return -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int (*p)(int);
+
+	p = cmp;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (p(array[i]) && array[i] != 0)
+			return (i);
+	}
+	return (-1);
+}
