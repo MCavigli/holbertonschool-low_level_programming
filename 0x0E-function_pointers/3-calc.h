1ef6d3fe5cfe21bbfa94e11948d0133564723277
Marc Cavigli <mcavigli@gmail.com>
2019-03-14 06:22:02 +0000

    Moved stuff around in order

--- 0x0E-function_pointers/3-calc.h
+++ 0x0E-function_pointers/3-calc.h
@@ -1,6 +1,12 @@
 #ifndef _3_CALC_H_
 #define _3_CALC_H_
-#include <stdio.h>
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+
 /**
  * struct op - Struct op
  *
@@ -12,10 +18,4 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
 #endif
