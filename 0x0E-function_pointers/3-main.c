10bb7fd1d5d85084909232d2c1ef3da23f733933
Marc Cavigli <mcavigli@gmail.com>
2019-03-14 20:06:31 +0000

    Moved conditional

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -10,16 +10,17 @@
 int main(int argc, char *argv[])
 {
 
-	int c;
+	int a, b, c;
 	int (*p)(int, int);
-	int a = atoi(argv[1]);
-	int b = atoi(argv[3]);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
 	if (argv[2][1] != '\0')
 	{
