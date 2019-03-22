30a539c48b48b77c9fd42823cecb0fd795864255
Marc Cavigli <mcavigli@gmail.com>
2019-03-22 05:18:26 +0000

    Pushed Task 4 to directory

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees a list_t list
+ * @head: the beginning of the list to free
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (temp != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
