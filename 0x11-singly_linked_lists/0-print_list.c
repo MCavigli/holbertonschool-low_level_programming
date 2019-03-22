686807ea2efd44943433cfd2060598e997a4dbcd
Marc Cavigli <mcavigli@gmail.com>
2019-03-22 05:58:05 +0000

    Added conditional to account for if h->str is NULL

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -11,7 +11,10 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%i] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", h->len, h->str);
 		h = h->next;
 		counter++;
 	}
