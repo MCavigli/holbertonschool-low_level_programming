a08ea164bb01b4910f53fba5fcb153dd387ebc0b
Marc Cavigli <mcavigli@gmail.com>
2019-03-26 20:11:19 +0000

    Added conditional for if temp is NULL

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,6 +35,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
+	if (!temp)
+		return (NULL);
 	if (temp->next == NULL)
 	{
 		new->n = n;
