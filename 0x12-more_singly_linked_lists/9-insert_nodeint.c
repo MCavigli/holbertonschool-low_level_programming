b195fdafdb1e1dd52a34d863bdd538fe19a072d4
Marc Cavigli <mcavigli@gmail.com>
2019-03-26 20:57:29 +0000

    Changed syntax of conditional

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,14 +24,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	if (temp)
 	{
-		if (idx == 0)
-		{
-			new->next = temp;
-			*head = new;
-		}
-		else if (temp->next == NULL)
+		if (temp->next == NULL)
 		{
 			new->next = NULL;
 			temp->next = new;
