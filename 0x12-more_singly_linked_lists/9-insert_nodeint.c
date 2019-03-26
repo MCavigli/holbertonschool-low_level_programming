fcf542fff855f139de1a084750fd1d2a0a5c7a28
Marc Cavigli <mcavigli@gmail.com>
2019-03-26 22:16:05 +0000

    Changed conditional statement

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
-	if (temp)
+	if (counter + 1 == idx)
 	{
 		if (temp->next == NULL)
 		{
