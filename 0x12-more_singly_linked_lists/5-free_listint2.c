92da1576c86e5c5aa559238f9787cc2d7d329c9e
Marc Cavigli <mcavigli@gmail.com>
2019-03-26 08:20:32 +0000

    Added conditional to check if head is NULL

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head)
+	if (head)
 	{
-		temp = *head;
-		*head = temp->next;
-		free(temp);
+		while (*head)
+		{
+			temp = *head;
+			*head = temp->next;
+			free(temp);
+		}
 	}
 	*head = NULL;
 }
