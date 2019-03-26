1143d7c0d7d399d6a7b3936e2b9bb85bb557ee26
Marc Cavigli <mcavigli@gmail.com>
2019-03-26 22:31:49 +0000

    Added to initial conditional statement

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 	unsigned int counter = 0;
 
-	if (temp == NULL)
+	if (temp == NULL && idx != 0)
 		return (NULL);
 
 	while (temp && counter < idx - 1)
