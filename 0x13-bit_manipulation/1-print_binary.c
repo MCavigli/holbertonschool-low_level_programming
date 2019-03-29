958f233e59c571b896600026890f2a2c6ea54adb
Marc Cavigli <mcavigli@gmail.com>
2019-03-29 01:14:05 +0000

    Pushed Task 1 to directory

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the number to convert
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	int c = 0;
+	int bit = sizeof(n) * 8 - 1;
+
+	while (bit >= 0)
+	{
+		if (n >> bit & 1)
+		{
+			_putchar('1');
+			c++;
+		}
+		else if (c)
+			_putchar('0');
+		bit--;
+	}
+}
