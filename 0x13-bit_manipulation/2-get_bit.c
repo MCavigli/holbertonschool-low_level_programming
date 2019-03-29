ed78aeb17e084fb8d36b94894b297dc18ac1e5ae
Marc Cavigli <mcavigli@gmail.com>
2019-03-29 02:04:58 +0000

    Pushed Task 2 to directory

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * get_bit - returns the value of a bit at the given index
+ * @n: the number to check
+ * @index: the index to check the bit
+ * Return: value of the bit at @index, -1 if there's an error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int bit = sizeof(n) * 8 - 1;
+
+	if (index > bit)
+		return (-1);
+
+	bit = bit >> index;
+
+	if (n & bit)
+		return (1);
+	else
+		return (0);
+}
