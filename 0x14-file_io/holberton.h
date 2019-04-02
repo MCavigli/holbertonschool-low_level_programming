9c06d4c0f3b7487a6ea3b215402f90b892cc2c43
Marc Cavigli <mcavigli@gmail.com>
2019-04-02 21:42:14 +0000

    Updated header

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -5,8 +5,10 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
 #endif
