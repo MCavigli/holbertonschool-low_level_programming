0ab13595f8eacac384d8f1d254a0cb07dfcbd617
Marc Cavigli <mcavigli@gmail.com>
2019-04-02 23:08:20 +0000

    Changed error message from integer to spelled out

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -13,25 +13,25 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	ffrom = open(argv[1], O_RDONLY);
 	if (ffrom  == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while ((rd = read(ffrom, buff, BUFSIZ)) > 0)
 		if (fto == -1 || (write(fto, buff, rd) != rd))
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	if (rd == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	clf = close(ffrom);
@@ -39,9 +39,9 @@ int main(int argc, char *argv[])
 	if (clf == -1 || clt == -1)
 	{
 		if (clf == -1)
-			dprintf(2, "Error: Can't close fd %d\n", ffrom);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ffrom);
 		else if (clt == -1)
-			dprintf(2, "Error: Can't close fd %d\n", fto);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fto);
 		exit(100);
 	}
 	return (0);
