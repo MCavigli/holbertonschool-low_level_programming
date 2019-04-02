0157689cc015eed741311b28b26714aaca227bd0
Marc Cavigli <mcavigli@gmail.com>
2019-04-02 20:58:15 +0000

    Added conditional for text_content == NULL

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -16,6 +16,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+	{
+		return (1);
+	}
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
