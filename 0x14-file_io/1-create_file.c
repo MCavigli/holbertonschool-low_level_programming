201b264fee213f46a48f318e806ea084b3079f9d
Marc Cavigli <mcavigli@gmail.com>
2019-04-02 20:50:56 +0000

    Altered argument for permissions

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -19,12 +19,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_WRONLY | O_CREAT, 600);
+		fd = open(filename, O_WRONLY | O_CREAT, 0600);
 		if (fd == -1)
 			return (-1);
 		return (1);
 	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
