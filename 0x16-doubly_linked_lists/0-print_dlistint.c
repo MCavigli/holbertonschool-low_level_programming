a288be546849bf48d4e2c9dc08b3de833dd344ef
Marc Cavigli <mcavigli@gmail.com>
2019-05-03 20:42:04 +0000

    Adding header file and Task 0 to directory

---
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements of a list
+ * @h: pointer to the head of the list
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
