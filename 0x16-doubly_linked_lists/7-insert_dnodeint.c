1229b5144b9a7b1b2da57442f09c941b3ccc81f3
Marc Cavigli <mcavigli@gmail.com>
2019-05-05 21:20:36 +0000

    Changed reassignment of the next node's previous link

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -51,7 +51,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	new->next = temp->next;
 	new->prev = temp;
-	temp->next->prev = new;
+	new->next->prev = new;
 	temp->next = new;
 	return (new);
 }
