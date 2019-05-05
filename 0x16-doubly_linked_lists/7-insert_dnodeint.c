366237f29683f78dde7c957eb4deb257ede73ba3
Marc Cavigli <mcavigli@gmail.com>
2019-05-05 22:25:13 +0000

    Moved check for if index is larger than list length outside of the loop

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,24 +34,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	size_t length = dlistint_len(*h);
 
 	new = malloc(sizeof(dlistint_t));
-	if (new == NULL || (h == NULL && idx != 0))
+	if (new == NULL || h == NULL)
+		return (NULL);
+	if (idx > length)
 		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	if (length == idx)
 		return (add_dnodeint_end(h, n));
+
 	while (counter < idx - 1)
 	{
 		temp = temp->next;
 		counter++;
-		if (temp->next == NULL)
-			return (NULL);
 	}
 
 	new->n = n;
 	new->next = temp->next;
 	new->prev = temp;
-	new->next->prev = new;
+	temp->next->prev = new;
 	temp->next = new;
 	return (new);
 }
