c15476d3e0b776f7c1757ef3f42fb84c0bdf1b1f
Marc Cavigli <mcavigli@gmail.com>
2019-05-31 23:49:19 +0000

    Pushed Task 4 to directory

---
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value assocaited with a key
+ * @ht: The hash table to look into
+ * @key: The key to look for
+ * Return: The value associated with the element, or NULL if key can't be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *ptr;
+	unsigned long int idx;
+
+	idx = key_index((unsigned char *)key, ht->size);
+	ptr = ht->array[idx];
+
+	while (ptr != NULL)
+	{
+		if (strcmp(ptr->key, key) == 0)
+			return (ptr->value);
+		ptr = ptr->next;
+	}
+	return (NULL);
+}
