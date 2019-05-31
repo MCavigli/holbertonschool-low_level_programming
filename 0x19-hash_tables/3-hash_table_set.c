024d9cb2e0c4d6c267aa643f6eb521621a88a209
Marc Cavigli <mcavigli@gmail.com>
2019-05-31 22:38:31 +0000

    Pushed Task 3 to directory

---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,53 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table
+ *
+ * @ht: The hash table you want to add or update the key/value to
+ * @key: The key to add. Cannot be an empty string
+ * @value: The value associated with the key. Must be a duplicate. Can be an
+ * empty string
+ *
+ * Return: 1 if success, 0 if failure
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *ptr;
+	hash_node_t *new_node;
+
+	unsigned long int idx = 0;
+
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
+
+	idx = key_index((unsigned char *)key, ht->size);
+	ptr = ht->array[idx];
+	while (ptr != NULL)
+	{
+		if (strcmp(ptr->key, key) == 0)
+		{
+			free(ptr->value);
+			ptr->value = strdup(value);
+			if (ptr->value == NULL)
+				return (0);
+			return (1);
+		}
+		ptr = ptr->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+		return (0);
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+		return (0);
+
+	new_node->next = ht->array[idx];
+	ht->array[idx] = new_node;
+	return (1);
+}
