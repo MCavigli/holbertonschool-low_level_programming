71fc9f2f085e438b4a1c308e343e870c83c34529
Marc Cavigli <mcavigli@gmail.com>
2019-06-01 00:42:02 +0000

    Pushed Task 6 to directory

---
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table
+ * Return: nothing
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int idx;
+	hash_node_t *ptr;
+	hash_node_t *prev;
+
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		ptr = ht->array[idx];
+		prev = ht->array[idx];
+		if (ptr == NULL)
+			continue;
+		while (ptr != NULL)
+		{
+			free(ptr->key);
+			free(ptr->value);
+			ptr = ptr->next;
+			free(prev);
+			prev = ptr;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
