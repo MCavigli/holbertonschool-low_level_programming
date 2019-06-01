31e4aab56011d09410f3a5f39d114e61d82a0f1a
Marc Cavigli <mcavigli@gmail.com>
2019-06-01 00:40:35 +0000

    Put free commands in correct order

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -45,8 +45,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->value = strdup(value);
 	if (new_node->value == NULL)
 	{
-		free(new_node);
 		free(new_node->key);
+		free(new_node);
 		return (0);
 	}
 	new_node->next = ht->array[idx];
