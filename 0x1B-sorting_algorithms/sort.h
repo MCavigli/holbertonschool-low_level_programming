ec4d40a611f18a1b5af91b7480a9a00eb7135062
Marc Cavigli <mcavigli@gmail.com>
2019-06-15 01:16:38 +0000

    Pushed header to directory

---
+++ 0x1B-sorting_algorithms/sort.h
@@ -0,0 +1,27 @@
+#ifndef __SORT__H__
+#define __SORT__H__
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/*---- Their functions ----*/
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
+/*---- Our functions ----*/
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+
+#endif
