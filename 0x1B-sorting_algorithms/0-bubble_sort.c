179c81ba7616bd0d7dce685cea0aacad2cf90657
Marc Cavigli <mcavigli@gmail.com>
2019-06-15 01:17:04 +0000

    Pushed Task 0 to directory

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order using the
+ * Bubble sort algorithm
+ *
+ * @array: Pointer to the beginning of an array
+ * @size: The size of the array
+ *
+ * Return: Nothing
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int flag = 1, tmp = 0;
+	size_t i;
+
+	if (size < 2)
+		return;
+
+	while (flag == 1)
+	{
+		flag = 0;
+
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+				flag = 1;
+			}
+		}
+	}
+}
