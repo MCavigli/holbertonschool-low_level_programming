987db1b7f262b18f25fadd43c5e94863188e8d01
Marc Cavigli <mcavigli@gmail.com>
2019-06-16 00:36:37 +0000

    Pushed Task 2 to directory

---
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+	int flag = 0, tmp = 0, place = 0;
+	size_t i, j;
+
+	if (!array)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		flag = 0;
+		tmp = 0;
+		for (j = i + 1; j < size; j++)
+		{
+			if (!flag && array[j] < array[i])
+			{
+				tmp = array[j];
+				place = j;
+				flag = 1;
+			}
+			if (flag && array[j] < tmp)
+			{
+				tmp = array[j];
+				place = j;
+			}
+		}
+		if (flag)
+		{
+			array[place] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
+		}
+	}
+}
