e7d0c96c60c37987dd29882f0983aa1309126921
Marc Cavigli <mcavigli@gmail.com>
2019-06-20 02:36:24 +0000

    Added prototype for counting sort

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -27,6 +27,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
 /*---- Our helper functions ----*/
 int checklist(listint_t **list, listint_t **current);
