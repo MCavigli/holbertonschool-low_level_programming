9bb39cf5f92d8730631491430062701fecd61113
Marc Cavigli <mcavigli@gmail.com>
2019-07-11 23:52:52 +0000

    Changed conditional to search for correct spot

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Module that holds the function def island_permiter'''
 
+
 def island_perimeter(grid):
     '''Finds the perimeter of an island'''
 
@@ -10,15 +11,13 @@ def island_perimeter(grid):
 
     for row in range(len(grid) - 1):
         for col in range(len(grid[row]) - 1):
-            if col == 1:
-                p += 4
-                if row > 0 and grid[row - 1][col] == 1:
-                    p -= 1
-                if col > 0 and grid[row][col - 1] == 1:
-                    p -= 1
-                if col < len(grid[row]) and grid[row][col + 1] == 1:
-                    p -= 1
-                if row < len(grid) and grid[row + 1][col] == 1:
-                    p -= 1
-
+            if grid[row][col] == 1:
+                if row > 0 and grid[row - 1][col] == 0:
+                    p += 1
+                if col > 0 and grid[row][col - 1] == 0:
+                    p += 1
+                if col < len(grid[row]) and grid[row][col + 1] == 0:
+                    p += 1
+                if row < len(grid) and grid[row + 1][col] == 0:
+                    p += 1
     return p
