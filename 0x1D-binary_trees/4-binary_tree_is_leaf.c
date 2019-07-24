0a9bb8fe8241757f98c555a9272ab38bff1ac916
Marc Cavigli <mcavigli@gmail.com>
2019-07-24 19:14:19 +0000

    Pushed Task 4 to directory

---
+++ 0x1D-binary_trees/4-binary_tree_is_leaf.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to the node to check
+ * Return: 1 if node is a leaf, 0 if it is not
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
