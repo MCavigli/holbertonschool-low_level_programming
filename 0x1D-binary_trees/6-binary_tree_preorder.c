c3b44b8f0bc6eef938d80d176757d123da5ccc66
Marc Cavigli <mcavigli@gmail.com>
2019-07-24 19:14:37 +0000

    Pushed Task 6 to directory

---
+++ 0x1D-binary_trees/6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - goes through a binary tree using pre-order traversal
+ * @tree: a pointer to the root node of the tree to traverse
+ * @func: a pointer to a function to call for each node. The value in the
+ * node must be passed as a parameter to this function.
+ * Return: nothing
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+}
