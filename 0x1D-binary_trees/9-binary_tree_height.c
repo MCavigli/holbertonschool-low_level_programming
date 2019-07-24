0c93277d9998c2b749a05f2d466f77d7b7068e64
Marc Cavigli <mcavigli@gmail.com>
2019-07-24 19:35:17 +0000

    Fixed betty issues

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -11,21 +11,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t rdepth;
 
 	if (!tree)
-                return (0);
+		return (0);
 
-        if (tree->left)
-                ldepth = binary_tree_height(tree->left) + 1;
-        else
-                ldepth = 0;
+	if (tree->left)
+		ldepth = binary_tree_height(tree->left) + 1;
+	else
+		ldepth = 0;
 
-        if (tree->right)
-                rdepth = binary_tree_height(tree->right) + 1;
-        else
-                rdepth = 0;
+	if (tree->right)
+		rdepth = binary_tree_height(tree->right) + 1;
+	else
+		rdepth = 0;
 
-        if (ldepth > rdepth)
-                return (ldepth);
-        else
-                return (rdepth);
-
+	if (ldepth > rdepth)
+		return (ldepth);
+	else
+		return (rdepth);
 }
