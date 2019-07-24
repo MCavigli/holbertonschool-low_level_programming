c7cbf0bc99bba4859f3d77c01486195a8aa921bd
Marc Cavigli <mcavigli@gmail.com>
2019-07-24 19:32:30 +0000

    Added condition to check if func == NULL

--- 0x1D-binary_trees/7-binary_tree_inorder.c
+++ 0x1D-binary_trees/7-binary_tree_inorder.c
@@ -9,10 +9,12 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL)
+	if (tree != NULL && func != NULL)
 	{
 		binary_tree_inorder(tree->left, func);
 		func(tree->n);
 		binary_tree_inorder(tree->right, func);
 	}
+	else
+		return;
 }
