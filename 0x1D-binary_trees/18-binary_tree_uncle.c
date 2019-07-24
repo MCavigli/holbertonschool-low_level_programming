394021b65ab2b9e8261437323a2440b0eadad339
Marc Cavigli <mcavigli@gmail.com>
2019-07-24 20:17:20 +0000

    Checked for left and right sides

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -10,10 +10,24 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
-	if (node->parent->right == node && node->parent->parent->right)
-		return (node->parent->parent->right);
-	else if (node->parent->left == node && node->parent->parent->left)
-		return (node->parent->parent->left);
-	else
-		return (NULL);
+	if (node->parent->right == node || node->parent->left == node)
+	{
+		if (node->parent->parent->right->right == node ||
+			node->parent->parent->right->left == node)
+		{
+			if (node->parent->parent->left)
+				return (node->parent->parent->left);
+		}
+		else if (node->parent->parent->left->right == node
+			 || node->parent->parent->left->left == node)
+		{
+			if (node->parent->parent->right)
+				return (node->parent->parent->right);
+		}
+		else
+			return (NULL);
+	}
+
+
+	return (NULL);
 }
