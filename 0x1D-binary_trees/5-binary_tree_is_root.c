aa7b7b563aebd4f740286c9f42d3d2ac723d54b1
Marc Cavigli <mcavigli@gmail.com>
2019-07-24 19:14:26 +0000

    Pushed Task 5 to directory

---
+++ 0x1D-binary_trees/5-binary_tree_is_root.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if node is a root
+ * @node: pointer to the node to check
+ * Return: 1 if node is a root, 0 if not or node is NULL
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node->parent == NULL)
+		return (1);
+	return (0);
+}
