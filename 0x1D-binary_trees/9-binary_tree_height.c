46050b6c5f24181bd6f36caca10a70281ec9f6a1
Marc Cavigli <mcavigli@gmail.com>
2019-07-24 19:15:03 +0000

    Pushed Task 9 to directory

---
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - finds the height of a particular node
+ * @tree: the node to find the height of
+ * Return: ldepth if it is the largest node length or rdepth if not
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t ldepth;
+	size_t rdepth;
+
+	if (!tree)
+                return (0);
+
+        if (tree->left)
+                ldepth = binary_tree_height(tree->left) + 1;
+        else
+                ldepth = 0;
+
+        if (tree->right)
+                rdepth = binary_tree_height(tree->right) + 1;
+        else
+                rdepth = 0;
+
+        if (ldepth > rdepth)
+                return (ldepth);
+        else
+                return (rdepth);
+
+}
