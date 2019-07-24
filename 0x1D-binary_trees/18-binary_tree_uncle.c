479f2d3d01c0ff1abb1b8a09cd40fcba8a1c5655
Marc Cavigli <mcavigli@gmail.com>
2019-07-24 20:28:53 +0000

    Added more conditions to check to make sure certain nodes even exist

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -12,14 +12,18 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 		return (NULL);
 	if (node->parent->right == node || node->parent->left == node)
 	{
-		if (node->parent->parent->right->right == node ||
-			node->parent->parent->right->left == node)
+		if ((node->parent->parent->right &&
+		     node->parent->parent->right->right) &&
+		    (node->parent->parent->right->right == node ||
+		    node->parent->parent->right->left == node))
 		{
 			if (node->parent->parent->left)
 				return (node->parent->parent->left);
 		}
-		else if (node->parent->parent->left->right == node
-			 || node->parent->parent->left->left == node)
+		else if ((node->parent->parent->left &&
+			  node->parent->parent->left->left) &&
+			 (node->parent->parent->left->right == node ||
+			  node->parent->parent->left->left == node))
 		{
 			if (node->parent->parent->right)
 				return (node->parent->parent->right);
