30336fa5ece5762c8a871f09a5af26965096a144
Marc Cavigli <mcavigli@gmail.com>
2019-07-24 19:24:37 +0000

    Changed order of conditional statement

--- 0x1D-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1D-binary_trees/4-binary_tree_is_leaf.c
@@ -7,8 +7,9 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
+	if (node == NULL)
+		return (0);
 	if (node->left == NULL && node->right == NULL)
 		return (1);
-	else
-		return (0);
+	return (0);
 }
