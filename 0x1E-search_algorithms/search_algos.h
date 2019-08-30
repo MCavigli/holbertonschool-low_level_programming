611d369e821ae7ab8eba7f7d9a661ced79053b23
Marc Cavigli <mcavigli@gmail.com>
2019-08-30 16:10:13 +0000

    Added prototype

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,4 +4,5 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int rec_bs(int *array, size_t size, int value, int low, int high);
+int advanced_binary(int *array, size_t size, int value);
 #endif
